145986b8209a4b5485aa89a0b699240579a3ee3b
English-For-Life <83182532+English-For-Life@users.noreply.github.com>
2021-08-08 22:34:04 +0300

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,26 @@
+//Array of objects
+const quiz = [
+
+    {
+        q:"Many young people hang ...... at shopping centres at weekends.",
+        options:["out", "on"],
+        answer: 0
+    },
+    {
+        q:"The police are running ...... a burglar.",
+        options:["into", "after"],
+        answer: 1
+    },
+    {
+        q:"There are lots of books to choose ...... .",
+        options:["from", "in"],
+        answer: 0
+    },
+    {
+        q:"He's crazy ...... football.",
+        options:["of", "about"],
+        answer: 1
+    }
+
+
+]
